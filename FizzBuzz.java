a5468d34a8159d7903f2d6a0d338c161094b4b9f
Armaan Damani <armaandamani@gmail.com>
2025-06-15 14:35:44 -0700

    final commit

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,1 +1,26 @@
+/*
+* FizzBuzz.java: Java program that prints numbers from 1 to 100, following the rules of FizzBuzz...
+*
+* @author Armaan Damani
+* @version 1.0
+* 
+*/
 
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Java-DSA-FizzBuzz
+
+A simple Java program that prints numbers from 1 to 100 with the following exceptions:
+
+- Multiples of 3 print `"Fizz"`
+- Multiples of 5 print `"Buzz"`
+- Multiples of both 3 and 5 print `"FizzBuzz"`
+- All other numbers print as-is
